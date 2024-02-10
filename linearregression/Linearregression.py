938cc1b676b2cd34c65e690667b62d26de08d4e5
Chen Yen Pin <chenyenpin1982@gmail.com>
2024-02-09 23:25:37 -0300

    Modified EDA, and Linearregression is working

--- linearregression/Linearregression.py
+++ linearregression/Linearregression.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np 
 
-class LinearRegressor():
+class LinearRegressor:
 
     def __init__(self, learning_rate=0.001, n_iter=1000) -> None:
         self.learning_rate = learning_rate
@@ -17,12 +17,18 @@ class LinearRegressor():
         for _ in range(self.n_iter):
             y_pred=np.dot(X, self.weigths) + self.bias
             #calculating derivates weights and bias
-            dW=(1/num_samples) * np.dot(X, (y_pred-y) )
+            dW=(1/num_samples) * np.dot(X.T, (y_pred-y) )
             db=(1/num_samples) * np.sum(y_pred-y)
             #updating weigths and bias 
-            self.weigths -=(self.learning_rate * dW)
-            self.bias-=(self.learning_rate * self.bias)
+            try:
+                self.weigths = self.weigths - (self.learning_rate * dW)
+                self.bias = self.bias - (self.learning_rate * db)
+            except RuntimeWarning as e:
+                print("Caught a runtime warning:", e)
 
     def predict(self, X):
          y_pred=np.dot(X, self.weigths) + self.bias
          return y_pred
+
+
+
